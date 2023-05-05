c2f1aab9fe788035275bb727f660776329e0a886
Thurinum <m.gagnon941@gmail.com>
2023-05-04 22:53:49 -0400

    feat: Remove accidental build files commited and add .gitignore

---
+++ lib/string.c
@@ -0,0 +1,43 @@
+#include <malloc.h>
+#include <memory.h>
+#include "string.h"
+
+void init_str(String* s, size_t initial_size) {
+    s->data = malloc(initial_size * sizeof(char*));
+    s->length = 0;
+    s->capacity = initial_size;
+}
+
+void append_to(String *v, char value) {
+    if (v->length == v->capacity) {
+        size_t new_capacity = v->capacity * 2;
+        char* tmp = realloc(v->data, new_capacity * sizeof(char*));
+
+        if (!tmp)
+            return;
+
+        v->data = tmp;
+        v->capacity *= new_capacity;
+    }
+
+    v->data[v->length++] = value;
+}
+
+void clear_str(String *s) {
+    s->data = malloc(sizeof(char*));
+    s->length = 0;
+    s->capacity = 1;
+}
+
+void free_str(String *s) {
+    free(s->data);
+    s->data = NULL;
+    s->length = s->capacity = 0;
+}
+
+const char* str(String *s) {
+    char* tmp = malloc((s->length + 1) * sizeof(char*));
+    memcpy(tmp, s->data, s->length);
+    tmp[s->length] = '\0';
+    return tmp;
+}
---
+++ lib/string.h
@@ -0,0 +1,18 @@
+#ifndef XML_PARSER_STRING_H
+#define XML_PARSER_STRING_H
+
+#include <corecrt.h>
+
+typedef struct {
+    char* data;
+    size_t length;
+    size_t capacity;
+} String;
+
+void init_str(String* s, size_t initial_size);
+const char* str(String *s);
+void append_to(String* s, char value);
+void clear_str(String* s);
+void free_str(String* s);
+
+#endif //XML_PARSER_STRING_H
